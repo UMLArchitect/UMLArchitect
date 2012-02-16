ce398e5cf557118422c234aaaf79153cb38511f2
Henri-Desobry <henri.desobry@gmail.com>
2012-02-16 17:16:47 +0100

    Add a class for the tests

--- UI/src/ihm/UITest.java
+++ UI/src/ihm/UITest.java
@@ -1,31 +1,24 @@
 package ihm;
 
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 
-import javax.swing.JComponent;
 import javax.swing.JFrame;
-import javax.swing.JWindow;
+import javax.swing.JPanel;
 
-public class UITest extends JWindow {
+public class UITest extends JFrame {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	public static void showOnFrame(JComponent component, String frameName) {
-		JFrame frame = new JFrame(frameName);
-		WindowAdapter wa = new WindowAdapter() {
-			public void windowClosing(WindowEvent e) {
-				System.exit(0);
-			}
-		};
-		frame.addWindowListener(wa);
-		frame.getContentPane().add(component);
-		frame.setUndecorated(true);
-		frame.setBounds(500, 300, 300, 300);
-		frame.pack();
-		frame.setVisible(true);
+	
+	public static void main(String[] args){
+		
+		JFrame ui = new UITest();	
+		JPanel panel = new JPanel();
+		ui.add(panel);		
+		ui.setSize(500, 300);
+		ui.setVisible(true);
+		
 	}
 	
 
