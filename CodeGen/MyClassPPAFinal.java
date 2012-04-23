022c53d13978f51f0e3ec466860373e82074fe19
thomasraynal <thomas.raynal2@gmail.com>
2012-04-23 10:41:56 +0200

    Velocity + fichiers de config

---
+++ CodeGen/MyClassPPAFinal.java
@@ -0,0 +1,14 @@
+package uml.classes;
+
+public static class Essai extends Object implements IUMLPackage, IUMLProject, IUML2, IUML3 {
+
+	public static String Champs1 = Valeur1;
+	public static int Champs2 = 500;
+
+	public static String M1(String Champs1, int Champs2){
+		// TODO - Auto generated code
+	}
+	public static void M2(){
+		// TODO - Auto generated code
+	}
+}
