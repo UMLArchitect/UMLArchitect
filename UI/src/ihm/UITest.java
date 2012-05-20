8e9cd09f7e22a8093961c256312a2cfab5945bcb
KILIC <KILIC@KILIC-PC>
2012-05-20 15:01:32 +0200

    New test case
    Change-Id: I1b6a315202b7d2ba33ebe864a8ce119fb7ce45b1

--- UI/src/ihm/UITest.java
+++ UI/src/ihm/UITest.java
@@ -1,10 +1,12 @@
 package ihm;
 
 
+import ihm.impl.diagrams.arrows.Arrow;
 import ihm.impl.diagrams.class_diagram.ClassDiagram_Class;
 import ihm.impl.listeners.MouseActions;
 
 import javax.swing.JFrame;
+
 
 /**
  * This class provides a general container to do some visual tests
@@ -25,23 +27,31 @@ public class UITest extends JFrame {
 		ui.setLayout(null);
 		ClassDiagram_Class panel = new ClassDiagram_Class();
 		ClassDiagram_Class panel2 = new ClassDiagram_Class();
+		ClassDiagram_Class panel3 = new ClassDiagram_Class();
+		ClassDiagram_Class panel4 = new ClassDiagram_Class();
 		panel.setBounds(100, 100, 100, 100);
 		panel2.setBounds(500, 500, 100, 100);
-
+		panel3.setBounds(100, 500, 100, 100);
+		panel4.setBounds(500, 100, 100, 100);
+		
+		Arrow arr= new Arrow(panel,panel2);
+		Arrow arr2= new Arrow(panel,panel3,3);
+		Arrow arr3= new Arrow(panel2,panel3,7);
+		Arrow arr4= new Arrow(panel2,panel4,8);
+		
 		ui.add(panel);
 		ui.add(panel2);
+		ui.add(panel3);
+		ui.add(panel4);
+		
+		ui.add(arr);
+		ui.add(arr2);
+		ui.add(arr3);
+		ui.add(arr4);
+		
 		ui.setSize(800, 800);
 		ui.setVisible(true);
 		ui.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-		MouseActions ma = new MouseActions();		
-		panel.addMouseMotionListener(ma);
-		panel.addMouseListener(ma);
-		panel2.addMouseMotionListener(ma);
-		panel2.addMouseListener(ma);
-		ma.add(panel);
-		ma.add(panel2);
-		
 		
 		
 	}
